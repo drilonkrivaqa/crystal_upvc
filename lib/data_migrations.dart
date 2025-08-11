3cb1f94bc3d5d27194ecfb0d6cc500d0b39c163d
drilonkrivaqa <drilonkrivaqa555@gmail.com>
2025-08-11 21:09:16 +0200

    Handle customer migration errors

--- lib/data_migrations.dart
+++ lib/data_migrations.dart
@@ -1,17 +1,28 @@
+import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
 import 'models.dart';
 
-Future<void> _migrateCustomers() async {
-  final box = await Hive.openBox<Customer>('customers');
-  for (final key in box.keys) {
-    final customer = box.get(key);
-    if (customer != null && (customer.email == null)) {
-      customer.email = '';
-      await customer.save();
+Future<bool> _migrateCustomers() async {
+  try {
+    final box = await Hive.openBox<Customer>('customers');
+    for (final key in box.keys) {
+      final customer = box.get(key);
+      if (customer != null && (customer.email == null)) {
+        customer.email = '';
+        await customer.save();
+      }
     }
+    return true;
+  } catch (e) {
+    debugPrint('Failed to migrate customers: $e');
+    return false;
   }
 }
 
-Future<void> runMigrations() async {
-  await _migrateCustomers();
+Future<List<String>> runMigrations() async {
+  final failures = <String>[];
+  if (!await _migrateCustomers()) {
+    failures.add('klientët');
+  }
+  return failures;
 }
