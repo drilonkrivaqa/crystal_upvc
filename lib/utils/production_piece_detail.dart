ce58fbcd195acfc5ea94314cac46c1b51e393a33
drilonkrivaqa <drilonkrivaqa555@gmail.com>
2025-10-07 20:51:41 +0200

    Add offer labels to cutting optimizer results

---
+++ lib/utils/production_piece_detail.dart
@@ -0,0 +1,11 @@
+class ProductionPieceDetail {
+  const ProductionPieceDetail({
+    required this.length,
+    required this.offerIndex,
+    required this.offerLabel,
+  });
+
+  final int length;
+  final int offerIndex;
+  final String offerLabel;
+}
