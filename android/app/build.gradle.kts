60313d4541243a652bc86c57c3c2af19d68efe5a
drilonkrivaqa <drilonkrivaqa555@gmail.com>
2025-10-09 19:57:51 +0200

    Revert "Update platform bundle identifiers to Toni Al-Pvc domain"

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.tonialpvc.crystal_upvc"
+    namespace = "com.example.crystal_upvc"
     compileSdk = flutter.compileSdkVersion
     // Override the default Flutter NDK with the version required by
     // image_picker and related plugins.
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.tonialpvc.crystal_upvc"
+        applicationId = "com.example.crystal_upvc"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
