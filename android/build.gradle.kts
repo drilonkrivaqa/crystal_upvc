d44063eb8ecd7399c8f18ac65e7ddaf0a3712c08
drilonkrivaqa <drilonkrivaqa555@gmail.com>
2025-10-26 00:34:26 +0200

    Sanitize Android build directories for Windows paths

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,13 @@
+import java.io.File
+
+fun File.removeEscapedSpaces(): File =
+    File(path.replace("\\ ", " "))
+
+val sanitizedRootBuildDir = File(rootProject.projectDir.absolutePath.replace("\\ ", " "), "build").removeEscapedSpaces()
+
+rootProject.buildDir = sanitizedRootBuildDir
+rootProject.layout.buildDirectory.set(sanitizedRootBuildDir)
+
 allprojects {
     repositories {
         google()
