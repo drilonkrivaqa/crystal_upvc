484431cdc8f28f9fd2781adf930bc921bdb4a82a
drilonkrivaqa <drilonkrivaqa555@gmail.com>
2025-11-02 17:50:52 +0100

    Add profile sorting preference and apply to window/door page

--- lib/pages/window_door_item_page.dart
+++ lib/pages/window_door_item_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/foundation.dart';
@@ -7,6 +8,7 @@ import '../models.dart';
 import '../theme/app_colors.dart';
 import 'window_door_designer_page.dart';
 import '../l10n/app_localizations.dart';
+import '../utils/profile_sort_order.dart';
 
 class WindowDoorItemPage extends StatefulWidget {
   final void Function(WindowDoorItem) onSave;
@@ -30,6 +32,7 @@ class _WindowDoorItemPageState extends State<WindowDoorItemPage> {
   late Box<Blind> blindBox;
   late Box<Mechanism> mechanismBox;
   late Box<Accessory> accessoryBox;
+  late Box settingsBox;
 
   late TextEditingController nameController;
   late TextEditingController widthController;
@@ -94,6 +97,7 @@ class _WindowDoorItemPageState extends State<WindowDoorItemPage> {
     blindBox = Hive.box<Blind>('blinds');
     mechanismBox = Hive.box<Mechanism>('mechanisms');
     accessoryBox = Hive.box<Accessory>('accessories');
+    settingsBox = Hive.box('settings');
 
     nameController =
         TextEditingController(text: widget.existingItem?.name ?? '');
@@ -410,23 +414,34 @@ class _WindowDoorItemPageState extends State<WindowDoorItemPage> {
                           Icons.view_list,
                         ),
                         const SizedBox(height: 16),
-                        DropdownButtonFormField<int>(
-                          initialValue: profileSetIndex,
-                          isExpanded: true,
-                          decoration:
-                              InputDecoration(labelText: l10n.catalogProfile),
-                          items: [
-                            for (int i = 0; i < profileSetBox.length; i++)
-                              DropdownMenuItem<int>(
-                                value: i,
-                                child: Text(
-                                  profileSetBox.getAt(i)?.name ?? '',
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                          ],
-                          onChanged: (val) =>
-                              setState(() => profileSetIndex = val ?? 0),
+                        ValueListenableBuilder(
+                          valueListenable: settingsBox.listenable(
+                            keys: [profileSortOrderSettingsKey],
+                          ),
+                          builder: (context, Box box, _) {
+                            final sortOrder =
+                                profileSortOrderFromStorage(box);
+                            final entries = getSortedProfileEntries(
+                                profileSetBox, sortOrder);
+                            return DropdownButtonFormField<int>(
+                              initialValue: profileSetIndex,
+                              isExpanded: true,
+                              decoration: InputDecoration(
+                                  labelText: l10n.catalogProfile),
+                              items: [
+                                for (final entry in entries)
+                                  DropdownMenuItem<int>(
+                                    value: entry.key,
+                                    child: Text(
+                                      entry.value.name,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                              ],
+                              onChanged: (val) => setState(
+                                  () => profileSetIndex = val ?? 0),
+                            );
+                          },
                         ),
                         const SizedBox(height: 16),
                         DropdownButtonFormField<int>(
