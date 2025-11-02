82936be491d920503b71e04411658792445429e1
drilonkrivaqa <drilonkrivaqa555@gmail.com>
2025-11-02 13:02:44 +0100

    Fix window designer repaint when layout changes

--- lib/pages/window_door_designer_page.dart
+++ lib/pages/window_door_designer_page.dart
@@ -1043,7 +1043,9 @@ class _WindowPainter extends CustomPainter {
         profileColor != old.profileColor ||
         blindColor != old.blindColor ||
         !_listEquals(cells, old.cells) ||
-        !_listEquals(cellGlassColors, old.cellGlassColors);
+        !_listEquals(cellGlassColors, old.cellGlassColors) ||
+        !_doubleListEquals(columnFractions, old.columnFractions) ||
+        !_doubleListEquals(rowFractions, old.rowFractions);
   }
 
   bool _listEquals(List a, List b) {
@@ -1051,6 +1053,15 @@ class _WindowPainter extends CustomPainter {
     if (a.length != b.length) return false;
     for (int i = 0; i < a.length; i++) {
       if (a[i] != b[i]) return false;
+    }
+    return true;
+  }
+
+  bool _doubleListEquals(List<double> a, List<double> b) {
+    if (identical(a, b)) return true;
+    if (a.length != b.length) return false;
+    for (int i = 0; i < a.length; i++) {
+      if ((a[i] - b[i]).abs() > 1e-6) return false;
     }
     return true;
   }
