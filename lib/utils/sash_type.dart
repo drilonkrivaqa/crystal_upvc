141f6e68ebe00e1da6a67a76ae964f785ab51b7c
Drilon Krivaqa <drilonkrivaqa555@gmail.com>
2025-12-29 00:05:24 +0100

    Fix double type usage in designer metrics

---
+++ lib/utils/sash_type.dart
@@ -0,0 +1,14 @@
+enum SashType {
+  fixed,
+  casementLeft,
+  casementRight,
+  tilt,
+  tiltLeft,
+  tiltRight,
+  tiltTurnLeft,
+  tiltTurnRight,
+  slidingLeft,
+  slidingRight,
+  slidingTiltLeft,
+  slidingTiltRight,
+}
